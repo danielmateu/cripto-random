d5a0bcdc4c8dbf73d7e0bb05edf7b3c169d55472
Daniel Mateu <danielmateu86@gmail.com>
2023-05-29 15:21:02 +0200

    Implementación de reactquery

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,38 @@
-
-import { useEffect, useReducer, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
+
+const getRandomNumberFromApi = async (): Promise<number> => {
+  const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new')
+  const numberString = await res.text()
+  if (!res.ok) {
+    throw new Error('Ha pasado algo...')
+  }
+
+  return parseInt(numberString)
+}
 
 function App() {
 
-  const getRandomNumberFromApi = async (): Promise<number> => {
-    const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new')
-
-    const numberString = await res.text()
-
-    // Si no podemos conectar, lanzamos un error
-    // throw new Error('Ha pasado algo...')
-    return parseInt(numberString)
-  }
-
-  const [number, setNumber] = useState<number>()
-  const [isLoading, setIsLoading] = useState<Boolean>(true)
-  const [error, setError] = useState<string>()
-  const [key, forceRefetch] = useReducer((x) => x + 1, 0)
-
-
-  useEffect(() => {
-    setIsLoading(true)
-    getRandomNumberFromApi()
-      .then(setNumber)
-      .catch(setError)
-  }, [key])
-
-  useEffect(() => {
-    if (number) {
-      setIsLoading(false)
-    }
-  }, [number])
-
-  useEffect(() => {
-    // Si hay un error
-    if (error) {
-      setIsLoading(false)
-    }
-  }, [error])
-
-
+  const query = useQuery(
+    ['randomNumber'],
+    getRandomNumberFromApi,
+  )
 
   return (
     <div className="App App-header">
       {
-        isLoading ?
-          <h2>Cargando...</h2> :
-          <h2>Número aleatorio: {number}</h2>
-        // Si no está cargando y hay un error
-
+        query.isFetching ? <h2>Cargando...</h2> : <h2>Número aleatorio: {query.data}</h2>
       }
 
       {
-        // Si hay un error y no está cargando
-        error && !isLoading &&
-        <h2>{error}</h2>
+        !query.isLoading && query.isError && <h2>{`${query.error}`}</h2>
       }
-      <button disabled={isLoading}
-        onClick={forceRefetch}
+      <button
+        onClick={() => query.refetch()}
+        disabled={query.isFetching}
       >
         {
-          isLoading ?
-            '...' :
-            'Nuevo número'
+          query.isFetching ? '...' : 'Nuevo número'
         }
       </button>
 
