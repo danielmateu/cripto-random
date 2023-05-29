80d2c61e7554ed8dd06258749ad2491b465c2f57
Daniel Mateu <danielmateu86@gmail.com>
2023-05-29 14:39:28 +0200

    obtención número aleatorio de forma tradicional

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const getRandomNumberFromApi = async (): Promise<number> => {
+    const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new')
+
+    const numberString = await res.text()
+    return parseInt(numberString)
+  }
+
+  const [number, setNumber] = useState<number>()
+
+  useEffect(() => {
+    getRandomNumberFromApi().then(num => setNumber(num))
+  }, [])
 
   return (
     <div className="App App-header">
-      <h2>Número aleatorio</h2>
+      <h2>Número aleatorio: {number}</h2>
     </div>
   )
 }
